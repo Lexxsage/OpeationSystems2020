db951b6fe9a88dadeea23a870e7f8bfa613fc5f2
Lexxsage <alexdevyatovskaya@gmail.com>
2020-10-08 15:15:21 +0700

    done

---
+++ 05 Search_Table/cmake-build-debug/CMakeFiles/05_Search_Table.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "05_Search_Table"
+  "05_Search_Table.pdb"
+  "CMakeFiles/05_Search_Table.dir/Search_Table.c.o"
+  "CMakeFiles/05_Search_Table.dir/main.c.o"
+  "CMakeFiles/05_Search_Table.dir/print_table_console.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/05_Search_Table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
