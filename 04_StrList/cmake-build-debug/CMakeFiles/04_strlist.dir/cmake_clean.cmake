3aae1920797fded1f1b676ae45f0508ce6d25945
alex <alexdevyatovskaya@gmail.com>
2020-10-09 09:19:21 +0700

    done

---
+++ 04_StrList/cmake-build-debug/CMakeFiles/04_strlist.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "04_strlist"
+  "04_strlist.pdb"
+  "CMakeFiles/04_strlist.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/04_strlist.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
