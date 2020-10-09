6ffdb316c512fda458f2da901d346397e567319c
alex <alexdevyatovskaya@gmail.com>
2020-10-09 12:36:09 +0700

    done

--- 03IdForFile/main.c
+++ 03IdForFile/main.c
@@ -1,50 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
 #include <errno.h>
+#include <sys/types.h> 
+#include <unistd.h>
 
-int open_file(char *filename)
-{
-    if (access(filename, F_OK) == -1)
-    {
-        perror(NULL);
-        return ENOENT; //error no entry
-    }
 
-    FILE *file = fopen(filename, "w"); // w - создает текстовый файл для записи
-    if (!file)
-    {
-        perror(NULL);
-        return errno; //code of the last error
-    }
-    printf("Open file: success.\n");
-    fclose(file);
+int main (int argc, char **argv){
 
-    return EXIT_SUCCESS;
-}
+        printf("uid %ld \n",getuid());
+        printf("eid %ld \n",geteuid());
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s [file]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
+        if (argc != 2) {
+                fprintf(stderr, "Need file\n");
+                return EXIT_FAILURE;
+        }
 
-    printf("uid = %d, ueid = %d\n", getuid(), geteuid());
+        FILE * f1;
+        f1 = fopen(argv[1], "r");
+        if (NULL == f1){ 
+perror("File not open");
+        }
 
-    open_file(argv[1]);
+        fclose(f1);
 
-    if (setuid(geteuid()) == -1)
-    {
-        fprintf(stderr, "Can't change euid\n");
-        perror(NULL);
-        return errno;
-    }
-    printf("Change euid: success.\n");
-    printf("uid = %d, ueid = %d\n", getuid(), geteuid());
+        uid_t uid = getuid();
+        setuid(uid);
+        printf("uid %ld \n",getuid());
+        printf("eid %ld \n",geteuid());
 
-    open_file(argv[1]);
+        f1 = fopen(argv[1],"r");
+        if (f1 == NULL){
+                perror("File not open");
+        }
 
-    return EXIT_SUCCESS;
+        fclose(f1);
+
+        return EXIT_SUCCES
 }
