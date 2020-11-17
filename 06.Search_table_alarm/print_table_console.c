7c9d84a867f9be5f114d2191e85d927a0aae20cb
lexxsage <alexdevyatovskaya@gmail.com>
2020-11-17 16:24:25 +0700

    update

--- 06.Search_table_alarm/print_table_console.c
+++ 06.Search_table_alarm/print_table_console.c
@@ -45,47 +45,49 @@ int print_table_console(int file_descriptor, Line_Record *search_table,
 
     if (retval>0)
     {
-      while (1)
-      {
-        int line_number, scanf_result;
-        char ending;
-        printf("$ ");
-        // On alarm, stops to wait user input.
-        scanf_result = scanf("%d%c", &line_number, &ending);
-        // User entered line number faster than SECOND_TO_COUNT.
-        // So alarm counting must to be stoped.
-        alarm(0);
-        if (is_timeout)
-        {
-            for (unsigned i = 0; i < search_table_size + 1; i++)
+        if (FD_ISSET(fd, &terminal) != 0) {
+            while (1)
             {
-                print_line(file_descriptor, search_table[i]);
+                int line_number, scanf_result;
+                char ending;
+                printf("$ ");
+                // On alarm, stops to wait user input.
+                scanf_result = scanf("%d%c", &line_number, &ending);
+                // User entered line number faster than SECOND_TO_COUNT.
+                // So alarm counting must to be stoped.
+                alarm(0);
+                if (is_timeout)
+                {
+                    for (unsigned i = 0; i < search_table_size + 1; i++)
+                    {
+                        print_line(file_descriptor, search_table[i]);
+                        putchar('\n');
+                    }
+                    exit(EXIT_SUCCESS);
+                }
+
+                if (scanf_result != 2 || ending != '\n')
+                {
+                    fprintf(stderr, "Format Error\n");
+                    return EXIT_FAILURE;
+                }
+
+                if (line_number == END_LINE_NUMBER)
+                {
+                    break;
+                }
+                if (line_number < 0 || line_number > search_table_size)
+                {
+                    fprintf(stderr, "Out Of Range Error: line_number %d out of range [%d, %d]\n",
+                            line_number, 1, search_table_size);
+                    continue;
+                }
+
+                print_line(file_descriptor, search_table[line_number]);
                 putchar('\n');
             }
-            exit(EXIT_SUCCESS);
+            return EXIT_SUCCESS;
         }
-
-        if (scanf_result != 2 || ending != '\n')
-        {
-            fprintf(stderr, "Format Error\n");
-            return EXIT_FAILURE;
-        }
-
-        if (line_number == END_LINE_NUMBER)
-        {
-            break;
-        }
-        if (line_number < 0 || line_number > search_table_size)
-        {
-            fprintf(stderr, "Out Of Range Error: line_number %d out of range [%d, %d]\n",
-                    line_number, 1, search_table_size);
-            continue;
-        }
-
-        print_line(file_descriptor, search_table[line_number]);
-        putchar('\n');
-    }
-    return EXIT_SUCCESS;
     }
     return 0;
 }
